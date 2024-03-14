18a2c40b25ebfcd9bad8ab128c63b39ca696e470
jsump <libbyn79@.com>
2024-03-14 13:59:12 +0000

    Add correct import module

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -7,10 +7,10 @@ the appropriate types
 """
 
 
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> [Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     this method returbs values with appropriate types
     """
