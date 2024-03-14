27e0133eb953cbdbfe677f21fbd4060cdf8fa96a
jsump <libbyn79@.com>
2024-03-14 12:25:11 +0000

    Retuen sum of list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module: 5-sum_list.py
+
+This module contains a function that returns sum of list as
+a float
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    this function takes a list input_list of floats as an
+    argument and returns their sum as float
+    """
+    return sum(input_list)
