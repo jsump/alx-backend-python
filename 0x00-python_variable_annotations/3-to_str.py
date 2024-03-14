603b25b12f5cf081783bd88c4602a7e6b53973ca
jsump <libbyn79@.com>
2024-03-14 12:05:52 +0000

    Return string representaion of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module: 3-to_str.py
+
+This module contains a funcion that returns the string representaion
+of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    This function takes a float n as an argument and returns
+    the string representation of the float
+    """
+    return str(n)
