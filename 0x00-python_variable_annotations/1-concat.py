d6f5cc256d78038519eaea7bcb8245717462bd16
jsump <libbyn79@.com>
2024-03-14 11:15:22 +0000

    Concationate 2 strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/binen python3
+"""
+Module: 1-concat.py
+
+This module returns a concatinated
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function takes a 2 strings and concates them
+    arguments: str1, str2
+    """
+    return str1 + str2
