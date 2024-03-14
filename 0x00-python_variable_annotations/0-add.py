95ef808de979320300c0959698ec8789825ace10
jsump <libbyn79@.com>
2024-03-14 11:06:04 +0000

    Return sum as float

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module:  0-add.py
+This module contains a function that returns a sum as float
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function takes float a and float b as arguments
+    Returns their sum as float
+    """
+    return a + b
