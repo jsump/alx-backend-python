a6d9e47d35dd570c9afa8d90ad6dcf79c5541f4f
jsump <libbyn79@.com>
2024-03-14 14:32:30 +0000

    Add type annotations and use TypeVar

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module: 101-safely_get_value.py
+
+This module contains a function that returns values with
+the appropriate types
+"""
+
+
+from typing import Mapping, Any, Union, Dict, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    this method returbs values with appropriate types
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
