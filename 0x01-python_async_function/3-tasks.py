83eb5b3d484a12e8504b55af57eb093fd47e0b9a
jsump <libbyn79@.com>
2024-03-18 17:39:00 +0000

    Take an integer and return asyncio.Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -2,9 +2,12 @@
 """
 Module: 3-tasks.py
 
-This module contains a function that returns asyncio.Task"""
+This module contains a function that returns asyncio.Task
+"""
 
 
+import random
+import asyncio
 from asyncio import Task
 from typing import Any
 from random import uniform
@@ -18,8 +21,8 @@ def task_wait_random(max_delay: int) -> Task:
     This method takes an integer max_delay and returns asyncio.Task
     """
     async def wait_random_wrapper(max_delay: int) -> Any:
-        delay = await wait_random(max_delay)
-        await sleep(delay)
-        return f"Waited for {delay: .2f} seconds"
+        delay = random.uniform(0, max_delay)
+        await asyncio.sleep(delay)
+        return delay
 
-    return wait_random_wrapper(max_delay)
+    return asyncio.create_task(wait_random_wrapper(max_delay))
