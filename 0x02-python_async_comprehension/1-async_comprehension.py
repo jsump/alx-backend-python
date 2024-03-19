9544315ceaa61d358343833287efe66fbf9ee433
jsump <libbyn79@.com>
2024-03-19 18:25:00 +0000

    Collect and return 10 random numbers using async comprehensing

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env pythn3
+"""
+Module: 1-async_comprehension.py
+
+This module contains a function that returns 10 random numbers
+using an async
+"""
+
+
+import asyncio
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+    This function takes no arguments.
+
+    This couritine will collect 10 random numbers
+    using async comprehension over async_generator,
+    then return the 10 random numberrs
+    """
+    random_floats = [num async for num in async_generator()]
+    return random_floats
