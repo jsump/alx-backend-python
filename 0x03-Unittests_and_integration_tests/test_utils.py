3fa3bd174d9a04fc157f061eebe1640ea0544e87
jsump <libbyn79@gmail.com>
2024-04-06 19:12:45 +0000

    Test the correct url, payload and mock response

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,8 +7,9 @@ First unit test for utils.access_nested_map
 
 
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -34,6 +35,30 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(expected_exception) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), expected_message)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    This class tests that utils.get_json returns the expected result
+    """
+    @parameterized.expand([
+        ("http://expample.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """
+        This method tests the urls, payload and mock requests
+        to be the expected results
+        """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_requests_get.return_value = mock_response
+
+        result = get_json(test_url)
+
+        mock_requests_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
