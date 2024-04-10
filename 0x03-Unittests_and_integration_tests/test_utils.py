974fb8489cfc5377a75ae4f2e4c9a5c8f495f4c6
jsump <libbyn79@gmail.com>
2024-04-10 12:15:01 +0000

    Edit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -88,8 +88,7 @@ class TestMemoize(unittest.TestCase):
                 """property"""
                 return lambda: self.a_method()
 
-        with patch.object(TestClass, 'a_method') as mock_method:
-            mock_method.return_value = 42
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
             test_instance = TestClass()
             result1 = test_instance.a_property()
             result2 = test_instance.a_property()
